4a4e89b4bea8a766b58ccc76b3a9e5b29093c31c
Stephen Loiola Bastos <stephen.loiola@actency.fr>
2024-06-03 17:52:39 +0200

    Add button for add block type + component created

---
+++ src/components/form/AddBlockView.tsx
@@ -0,0 +1,149 @@
+import React, {useCallback, useEffect, useMemo} from "react";
+import styled from "styled-components";
+import {useAppDispatch} from "../../redux/store";
+import {addBlockTextImage, addBlockMultipleImages} from "../../redux/ArticleSlice";
+import { IoCloseOutline } from "react-icons/io5";
+
+
+const StyledAddBlockView = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #282c3490;
+    z-index: 1000;
+    .select-new-block-widget {
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        border-radius: var(--border-radius);
+        min-height: 20rem;
+        max-height: 80vh;
+        width: min(40rem, 90vw);
+        header {
+            position: relative;
+            padding: 1rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 3rem;
+            background-color: #282c34;
+            h3 {
+                text-transform: uppercase;
+                color: white;
+                padding-right: 3rem;
+                line-height: 1.5;
+                text-align: center;
+            }
+            button {
+                position: absolute;
+                right: 1rem;
+                top: 50%;
+                transform: translateY(-50%);
+                svg {
+                    color: whitesmoke;
+                }
+            }
+        }
+        section {
+            position: relative;
+            flex: 1;
+            padding: 1rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            ul {
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                align-items: center;
+                flex-wrap: wrap;
+                gap: 1rem;
+                li {
+                    button {
+                        font-size: 1.125rem;
+                        color: white;
+                        height: 10rem;
+                        width: 10rem;
+                        background-color: gray;
+                        border-radius: var(--border-radius);
+                        padding: 0.5rem;
+                    }
+                }
+            }
+        }
+    }
+`;
+
+interface AddImagesProps extends React.HTMLProps<HTMLInputElement> {
+    buttonList?: string[],
+    openViewState: {state: boolean, set: React.Dispatch<boolean>}
+}
+
+const AddBlockView = (props: AddImagesProps) => {
+
+    type TBlocksNames = 'Textes + Images' | 'Slider Images';
+
+    const listBlocksType: TBlocksNames[] = ['Textes + Images', 'Slider Images'];
+    const dispatch = useAppDispatch()
+
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        if(!props.openViewState.state) {
+            document.body.style.overflow = "scroll"
+        }
+    }, []);
+
+    const closeView = useCallback(() => {
+        document.body.style.overflow = "initial";
+        props.openViewState.set(false);
+    }, [props.openViewState])
+
+    const addBlock = useCallback((blockName: TBlocksNames) => {
+        switch (blockName) {
+            case 'Textes + Images':
+                dispatch(addBlockTextImage());
+                closeView()
+                break;
+            case 'Slider Images':
+                dispatch(addBlockMultipleImages());
+                closeView()
+                break;
+            default:
+                return
+        }
+    },[])
+
+
+    return (
+        <StyledAddBlockView>
+            <div className={'select-new-block-widget'}>
+                <header>
+                    <h3>Choissisez un bloc à ajouter</h3>
+                    <button
+                        onClick={() => closeView()}
+                    >
+                        <IoCloseOutline size={40}/>
+                    </button>
+                </header>
+                <section>
+                <ul>
+                        {listBlocksType.map((e, i) =>
+                            <li key={i}>
+                                <button onClick={() => addBlock(e)}>{e}</button>
+                            </li>
+                        )}
+                    </ul>
+                </section>
+            </div>
+        </StyledAddBlockView>
+    )
+}
+
+export default React.memo(AddBlockView);
--- src/components/form/BlockList.tsx
+++ src/components/form/BlockList.tsx
@@ -9,160 +9,181 @@ import BlockReorder from "./BlockReorder";
 import BlockDefault from "./BlockDefault";
 import BlockListInfo from "./BlockListInfo";
 import ImagesBlock from "./ImagesBlock";
+import AddBlockView from "./AddBlockView";
 
 const StyledBlockList = styled.section<{ $reOrderView: boolean }>`
 
-  .paragraph-plus-images {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(35rem, 0.5fr));
-    grid-gap: 2rem;
+    .paragraph-plus-images {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(35rem, 0.5fr));
+        grid-gap: 2rem;
 
-    .paragraph-form-container {
-      background-color: var(--white);
-      padding: 1rem;
-      display: grid;
-      height: 38rem;
-      border-radius: var(--border-radius);
-      grid-template-columns: repeat(9, 1fr);
-      grid-template-rows: repeat(12, 1fr);
-      gap: 1rem;
+        .paragraph-form-container {
+            background-color: var(--white);
+            padding: 1rem;
+            display: grid;
+            height: 38rem;
+            border-radius: var(--border-radius);
+            grid-template-columns: repeat(9, 1fr);
+            grid-template-rows: repeat(12, 1fr);
+            gap: 1rem;
 
 
-      &:hover {
-        .button-plus-container,
-        .button-minus-container,
-        .paragraph-number-container {
-          visibility: visible;
+            &:hover {
+                .button-plus-container,
+                .button-minus-container,
+                .paragraph-number-container {
+                    visibility: visible;
+                }
+            }
+
+            > * {
+                &:nth-child(1) {
+                    grid-area: 1 / 1 / 3 / 9;
+                }
+
+                &:nth-child(2) {
+                    grid-area: 3 / 1 / 10 / 9;
+                }
+
+                &:nth-child(3) {
+                    grid-area: 10 / 1 / 13 / 9;
+                }
+
+                &:nth-child(4) {
+                    grid-area: 1 / 9 / 5 / 10;
+                }
+
+                &:nth-child(5) {
+                    grid-area: 5 / 9 / 9 / 10;
+                }
+
+                &:nth-child(6) {
+                    grid-area: 9 / 9 / 13 / 10;
+                }
+            }
+
+            .paragraph-form-button {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                    //border: 2px solid ${props => props.theme.mainColor};
+                border-radius: var(--border-radius);
+                //background-color: var(--light-gray);
+
+                svg {
+                    height: 3rem;
+                    width: 3rem;
+                    color: ${props => props.theme.mainColor};
+                }
+            }
+
+            .button-plus-container {
+                svg {
+                    color: var(--blue);
+                }
+            }
+
+            .button-minus-container {
+                svg {
+                    color: var(--red);
+                }
+            }
+
+            .icon-not-clickable {
+                svg {
+                    color: var(--gray);
+                }
+            }
+
+            .paragraph-number-container {
+                border: none;
+                font-size: 2.5rem;
+                cursor: inherit;
+                background-color: inherit;
+
+                &:hover {
+                    background-color: inherit;
+                }
+            }
         }
-      }
+    }
 
-      > * {
-        &:nth-child(1) {
-          grid-area: 1 / 1 / 3 / 9;
-        }
-
-        &:nth-child(2) {
-          grid-area: 3 / 1 / 10 / 9;
-        }
-
-        &:nth-child(3) {
-          grid-area: 10 / 1 / 13 / 9;
-        }
-
-        &:nth-child(4) {
-          grid-area: 1 / 9 / 5 / 10;
-        }
-
-        &:nth-child(5) {
-          grid-area: 5 / 9 / 9 / 10;
-        }
-
-        &:nth-child(6) {
-          grid-area: 9 / 9 / 13 / 10;
-        }
-      }
-
-      .paragraph-form-button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-          //border: 2px solid ${props => props.theme.mainColor};
+    .add-block-item {
+        position: relative;
+        border: 2px dashed white;
         border-radius: var(--border-radius);
-        //background-color: var(--light-gray);
+        overflow: hidden;
 
-        svg {
-          height: 3rem;
-          width: 3rem;
-          color: ${props => props.theme.mainColor};
+        .add-block-button {
+            padding: 1rem;
+            background-color: white;
+
+            &:before {
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+            }
         }
-      }
-
-      .button-plus-container {
-        svg {
-          color: var(--blue);
-        }
-      }
-
-      .button-minus-container {
-        svg {
-          color: var(--red);
-        }
-      }
-
-      .icon-not-clickable {
-        svg {
-          color: var(--gray);
-        }
-      }
-
-      .paragraph-number-container {
-        border: none;
-        font-size: 2.5rem;
-        cursor: inherit;
-        background-color: inherit;
-
-        &:hover {
-          background-color: inherit;
-        }
-      }
-    }
-  }
-
-  .toggle-button-container {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    align-items: center;
-    gap: 1rem;
-    font-size: 1.1rem;
-    margin-bottom: 1rem;
-  }
-
-  .list-container {
-    background-color: #282c34;
-    border-radius: var(--border-radius);
-    padding: 1.5rem;
-
-    .reorder-container-title {
-      padding: 1rem 0;
-      margin-bottom: 1rem;
-
-      label {
-        color: var(--white);
-        font-size: var(--xlarge);
-      }
-
-      img, svg {
-        height: 50px;
-        width: 50px;
-        color: white;
-      }
     }
 
-    .block-list-reorder {
-      display: flex !important;
-      flex-direction: row !important;
-      flex-wrap: wrap;
-      gap: 2rem;
-
-      .reorder-item {
-        flex: 0 0 calc(33.333% - 3rem);
-      }
+    .toggle-button-container {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        align-items: center;
+        gap: 1rem;
+        font-size: 1.1rem;
+        margin-bottom: 1rem;
     }
-  }
 
-  > label {
-    display: flex;
-    gap: 0.4rem;
+    .list-container {
+        background-color: #282c34;
+        border-radius: var(--border-radius);
+        padding: 1.5rem;
 
-    button {
-      transition: 0.2s ease-in-out;
+        .reorder-container-title {
+            padding: 1rem 0;
+            margin-bottom: 1rem;
 
-      &:hover {
-        transform: scale(1.2);
-      }
+            label {
+                color: var(--white);
+                font-size: var(--xlarge);
+            }
+
+            img, svg {
+                height: 50px;
+                width: 50px;
+                color: white;
+            }
+        }
+
+        .block-list-reorder {
+            display: flex !important;
+            flex-direction: row !important;
+            flex-wrap: wrap;
+            gap: 2rem;
+
+            .reorder-item {
+                flex: 0 0 calc(33.333% - 3rem);
+            }
+        }
     }
-  }
+
+    > label {
+        display: flex;
+        gap: 0.4rem;
+
+        button {
+            transition: 0.2s ease-in-out;
+
+            &:hover {
+                transform: scale(1.2);
+            }
+        }
+    }
 `;
 
 interface StyledBlockListProps extends React.HTMLProps<HTMLElement> {
@@ -175,7 +196,7 @@ const BlockList = ({label, ...props}: StyledBlockListProps) => {
     const dispatch = useAppDispatch()
 
     const [reOrderView, setReOrderView] = useState(false);
-
+    const [openAddBlockView, setOpenAddBlockView] = useState<boolean>(false)
 
     type TBlockListView = 'default' | 'reorder' | 'info';
 
@@ -188,18 +209,6 @@ const BlockList = ({label, ...props}: StyledBlockListProps) => {
         {title: "Détail", callBack: () => setBlockListView('info')},
     ]
 
-    /**
-     * 'default': <ul
-     *                             className={"paragraph-plus-images"}
-     *                         >
-     *                             {paragraphData && paragraphData.map((e, i) => (
-     *                                 <BlockDefault i={i} e={e}  key={e.index}/>
-     *                             ))}
-     *                         </ul>,
-     */
-
-
-    //                                <ImagesBlock i={i} e={e}  key={i}/>
     return (
         <StyledBlockList $reOrderView={reOrderView} {...props}>
             <label>{label}</label>
@@ -211,13 +220,21 @@ const BlockList = ({label, ...props}: StyledBlockListProps) => {
 
                 {
                     {
-                        'default': <ul
-                            className={"paragraph-plus-images"}
-                        >
-                            {paragraphData && paragraphData.map((e, i) => (
-                                <BlockDefault i={i} e={e} />
-                            ))}
-                        </ul>,
+                        'default': <>
+                            <ul className={"paragraph-plus-images"}>
+                                {paragraphData && paragraphData.map((e, i) => (
+                                    <BlockDefault i={i} e={e}/>
+                                ))}
+                                <li className={'add-block-item'}>
+                                    <button
+                                        className={'add-block-button'}
+                                        onClick={() => setOpenAddBlockView(true)}
+                                    >
+                                        Ajouter un block
+                                    </button>
+                                </li>
+                            </ul>
+                        </>,
                         'reorder': <Reorder.Group
                             values={paragraphData}
                             onReorder={e => dispatch(update({article: e}))}
@@ -232,6 +249,7 @@ const BlockList = ({label, ...props}: StyledBlockListProps) => {
                     }[blockListView]
                 }
             </div>
+            {openAddBlockView && <AddBlockView openViewState={{ state: openAddBlockView, set: setOpenAddBlockView}}/>}
         </StyledBlockList>
     )
 }
